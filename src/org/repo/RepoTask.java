bd4a42e94e42043bb591cbcb3fd3173f35a065a9
sharmi101 <sharmilamohan710@gmail.com>
2022-11-22 14:48:22 +0530

    new

--- src/org/repo/RepoTask.java
+++ src/org/repo/RepoTask.java
@@ -2,13 +2,13 @@ package org.repo;
 
 public class RepoTask {
 	public static void main(String[] args) {
+		System.out.println("7");
+		System.out.println("1");
 		System.out.println("z");
 		System.out.println("v");
-<<<<<<< HEAD
-		System.out.println("7");
-=======
-		System.out.println("1");
->>>>>>> 565c776f9f4cef0a4d50f9f54ac8223b1838f5de
+
+	
+
 	}
 
 }
